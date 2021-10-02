3254f3eb343482eb1e556b8e0bd3208ce0aa0556
Ian <iantdwalker@gmail.com>
2021-10-02 10:55:16 +0100

    Added AsyncExamples project

---
+++ AsyncAwaitTest/MainForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace AsyncAwaitTest
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this._buttonTest = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this._textBoxValue = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// _buttonTest
+			// 
+			this._buttonTest.Location = new System.Drawing.Point(174, 74);
+			this._buttonTest.Name = "_buttonTest";
+			this._buttonTest.Size = new System.Drawing.Size(75, 23);
+			this._buttonTest.TabIndex = 0;
+			this._buttonTest.Text = "Test";
+			this._buttonTest.UseVisualStyleBackColor = true;
+			this._buttonTest.Click += new System.EventHandler(this._buttonTestClick);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(93, 74);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(12, 74);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 48);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// _textBoxValue
+			// 
+			this._textBoxValue.Location = new System.Drawing.Point(12, 22);
+			this._textBoxValue.Name = "_textBoxValue";
+			this._textBoxValue.Size = new System.Drawing.Size(100, 20);
+			this._textBoxValue.TabIndex = 4;
+			this._textBoxValue.Text = "Value here!";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(118, 22);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 5;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(118, 48);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 6;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(263, 109);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this._textBoxValue);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this._buttonTest);
+			this.Name = "MainForm";
+			this.Text = "AsyncAwaitTest";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox _textBoxValue;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button _buttonTest;
+	}
+}
---
+++ AsyncAwaitTest/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwaitTest
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		private async void _buttonTestClick(object sender, EventArgs e)
+		{
+			int highValue = int.Parse(_textBoxValue.Text);
+			for (int x = 0; x < highValue; x++)
+			{
+				var newValue = await CalculateValueAsync(x);
+				_textBoxValue.Text = newValue.ToString();
+			}
+		}
+		
+		private async Task<int> CalculateValueAsync(int number)
+		{
+			await Task.Delay(3000);
+			return number * 10;
+		}		
+	}
+}
